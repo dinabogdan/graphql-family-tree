d94eaca271f05bdc4e346c0fcc2dfa18c3c05b5d
Bogdan.Dina <bogdan.dina@orange.com>
2018-06-25 09:31:34 +0300

    change to gradle

---
+++ src/main/kotlin/com/freesoft/graphqlfamilytree/GraphqlFamilyTreeApplication.kt
@@ -0,0 +1,11 @@
+package com.freesoft.graphqlfamilytree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlFamilyTreeApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlFamilyTreeApplication>(*args)
+}
